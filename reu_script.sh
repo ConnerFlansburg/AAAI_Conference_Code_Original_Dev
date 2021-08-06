156413d2d8701602861c57a6b01e03e15e30353c
connerf <connerflansburg93@gmail.com>
2021-08-06 13:39:55 -0500

    now runs neural net & plot

--- reu_script.sh
+++ reu_script.sh
@@ -4,3 +4,9 @@
 python3 dRegression.py -c kdfw  # run with the city kdfw
 python3 dRegression.py -c kcys  # run with the city kcys
 python3 dRegression.py -c kroa  # run with the city kroa
+
+python3 neural_net.py -c kdfw   # run neural network with the city kdfw
+python3 neural_net.py -c kcys   # run neural network with the city kcys
+python3 neural_net.py -c kroa   # run neural network with the city kroa
+
+python3 create_plot.py          # create the plots for each city
